9e16b5d8a9fa07d8f45c296081b2108c0751f518
liweijie <liweijie@caixiaomi.com>
2018-04-25 15:10:43 +0800

    阈值设置模块
    Change-Id: Ia1d3c8b6b4f409309a72a2a0249dbff206c4a163
    Signed-off-by: Mr.Li <Mr.Li@windows10.microdone.cn>

---
+++ src/main/java/com/fh/service/lottery/thresholdvalue/impl/ThresholdValueService.java
@@ -0,0 +1,100 @@
+package com.fh.service.lottery.thresholdvalue.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.fh.dao.DaoSupport3;
+import com.fh.entity.Page;
+import com.fh.service.lottery.thresholdvalue.ThresholdValueManager;
+import com.fh.util.PageData;
+
+/**
+ * 说明： 阈值设置 创建人：FH Q313596790 创建时间：2018-04-25
+ * 
+ * @version
+ */
+@Service("thresholdvalueService")
+public class ThresholdValueService implements ThresholdValueManager {
+
+	@Resource(name = "daoSupport3")
+	private DaoSupport3 dao;
+
+	/**
+	 * 新增
+	 * 
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd) throws Exception {
+		dao.save("ThresholdValueMapper.save", pd);
+	}
+
+	/**
+	 * 删除
+	 * 
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd) throws Exception {
+		dao.delete("ThresholdValueMapper.delete", pd);
+	}
+
+	/**
+	 * 修改
+	 * 
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd) throws Exception {
+		dao.update("ThresholdValueMapper.edit", pd);
+	}
+
+	/**
+	 * 列表
+	 * 
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page) throws Exception {
+		return (List<PageData>) dao.findForList(
+				"ThresholdValueMapper.datalistPage", page);
+	}
+
+	/**
+	 * 列表(全部)
+	 * 
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd) throws Exception {
+		return (List<PageData>) dao.findForList("ThresholdValueMapper.listAll",
+				pd);
+	}
+
+	/**
+	 * 通过id获取数据
+	 * 
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd) throws Exception {
+		return (PageData) dao
+				.findForObject("ThresholdValueMapper.findById", pd);
+	}
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS) throws Exception {
+		dao.delete("ThresholdValueMapper.deleteAll", ArrayDATA_IDS);
+	}
+
+}
