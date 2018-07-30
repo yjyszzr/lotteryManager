c9250ec1ca2ceadd988568085653eb8be57801e6
liweijie <liweijie@caixiaomi.com>
2018-07-30 18:21:32 +0800

    添加彩种管理模块

---
+++ src/main/java/com/fh/controller/lottery/lotteryclassify/LotteryClassifyController.java
@@ -0,0 +1,249 @@
+package com.fh.controller.lottery.lotteryclassify;
+
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.service.lottery.lotteryclassify.LotteryClassifyManager;
+import com.fh.util.AppUtil;
+import com.fh.util.DateUtilNew;
+import com.fh.util.Jurisdiction;
+import com.fh.util.ObjectExcelView;
+import com.fh.util.PageData;
+
+/**
+ * 说明：彩种管理 创建人：FH Q313596790 创建时间：2018-07-30
+ */
+@Controller
+@RequestMapping(value = "/lotteryclassify")
+public class LotteryClassifyController extends BaseController {
+
+	String menuUrl = "lotteryclassify/list.do"; // 菜单地址(权限用)
+	@Resource(name = "lotteryclassifyService")
+	private LotteryClassifyManager lotteryclassifyService;
+
+	/**
+	 * 保存
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/save")
+	public ModelAndView save() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "新增LotteryClassify");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "add")) {
+			return null;
+		} // 校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		// pd.put("lottery_classify_id", this.get32UUID()); //主键
+		pd.put("lottery_classify_id", "0"); // id
+		pd.put("create_time", DateUtilNew.getCurrentTimeLong()); // 创建时间
+		lotteryclassifyService.save(pd);
+		mv.addObject("msg", "success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+
+	/**
+	 * 删除
+	 * 
+	 * @param out
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/delete")
+	public void delete(PrintWriter out) throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "删除LotteryClassify");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "del")) {
+			return;
+		} // 校验权限
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		lotteryclassifyService.delete(pd);
+		out.write("success");
+		out.close();
+	}
+
+	/**
+	 * 修改
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/edit")
+	public ModelAndView edit() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "修改LotteryClassify");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "edit")) {
+			return null;
+		} // 校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		lotteryclassifyService.edit(pd);
+		mv.addObject("msg", "success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+
+	/**
+	 * 列表
+	 * 
+	 * @param page
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/list")
+	public ModelAndView list(Page page) throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "列表LotteryClassify");
+		// if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;}
+		// //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = pd.getString("keywords"); // 关键词检索条件
+		if (null != keywords && !"".equals(keywords)) {
+			pd.put("keywords", keywords.trim());
+		}
+		page.setPd(pd);
+		List<PageData> varList = lotteryclassifyService.list(page); // 列出LotteryClassify列表
+		mv.setViewName("lottery/lotteryclassify/lotteryclassify_list");
+		mv.addObject("varList", varList);
+		mv.addObject("pd", pd);
+		mv.addObject("QX", Jurisdiction.getHC()); // 按钮权限
+		return mv;
+	}
+
+	/**
+	 * 去新增页面
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/goAdd")
+	public ModelAndView goAdd() throws Exception {
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		mv.setViewName("lottery/lotteryclassify/lotteryclassify_edit");
+		mv.addObject("msg", "save");
+		mv.addObject("pd", pd);
+		return mv;
+	}
+
+	/**
+	 * 去修改页面
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/goEdit")
+	public ModelAndView goEdit() throws Exception {
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd = lotteryclassifyService.findById(pd); // 根据ID读取
+		mv.setViewName("lottery/lotteryclassify/lotteryclassify_edit");
+		mv.addObject("msg", "edit");
+		mv.addObject("pd", pd);
+		return mv;
+	}
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/deleteAll")
+	@ResponseBody
+	public Object deleteAll() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "批量删除LotteryClassify");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "del")) {
+			return null;
+		} // 校验权限
+		PageData pd = new PageData();
+		Map<String, Object> map = new HashMap<String, Object>();
+		pd = this.getPageData();
+		List<PageData> pdList = new ArrayList<PageData>();
+		String DATA_IDS = pd.getString("DATA_IDS");
+		if (null != DATA_IDS && !"".equals(DATA_IDS)) {
+			String ArrayDATA_IDS[] = DATA_IDS.split(",");
+			lotteryclassifyService.deleteAll(ArrayDATA_IDS);
+			pd.put("msg", "ok");
+		} else {
+			pd.put("msg", "no");
+		}
+		pdList.add(pd);
+		map.put("list", pdList);
+		return AppUtil.returnObject(pd, map);
+	}
+
+	/**
+	 * 导出到excel
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/excel")
+	public ModelAndView exportExcel() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "导出LotteryClassify到excel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "cha")) {
+			return null;
+		}
+		ModelAndView mv = new ModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		List<String> titles = new ArrayList<String>();
+		titles.add("id"); // 1
+		titles.add("彩种名称"); // 2
+		titles.add("彩种logo"); // 3
+		titles.add("排序"); // 4
+		titles.add("状态"); // 5
+		titles.add("是否展示"); // 6
+		titles.add("创建时间"); // 7
+		titles.add("跳转链接"); // 8
+		dataMap.put("titles", titles);
+		List<PageData> varOList = lotteryclassifyService.listAll(pd);
+		List<PageData> varList = new ArrayList<PageData>();
+		for (int i = 0; i < varOList.size(); i++) {
+			PageData vpd = new PageData();
+			vpd.put("var1", varOList.get(i).get("lottery_classify_id").toString()); // 1
+			vpd.put("var2", varOList.get(i).getString("lottery_name")); // 2
+			vpd.put("var3", varOList.get(i).getString("lottery_img")); // 3
+			vpd.put("var4", varOList.get(i).get("sort").toString()); // 4
+			vpd.put("var5", varOList.get(i).get("status").toString()); // 5
+			vpd.put("var6", varOList.get(i).get("is_show").toString()); // 6
+			vpd.put("var7", varOList.get(i).get("create_time").toString()); // 7
+			vpd.put("var8", varOList.get(i).getString("redirect_url")); // 8
+			varList.add(vpd);
+		}
+		dataMap.put("varList", varList);
+		ObjectExcelView erv = new ObjectExcelView();
+		mv = new ModelAndView(erv, dataMap);
+		return mv;
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format, true));
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/lottery/lotteryclassify/lotteryclassify_edit.jsp
@@ -0,0 +1,203 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<base href="<%=basePath%>">
+	<!-- 下拉框 -->
+	<link rel="stylesheet" href="static/ace/css/chosen.css" />
+	<!-- jsp文件头和头部 -->
+	<%@ include file="../../system/index/top.jsp"%>
+	<!-- 日期框 -->
+	<link rel="stylesheet" href="static/ace/css/datepicker.css" />
+</head>
+<body class="no-skin">
+<!-- /section:basics/navbar.layout -->
+<div class="main-container" id="main-container">
+	<!-- /section:basics/sidebar -->
+	<div class="main-content">
+		<div class="main-content-inner">
+			<div class="page-content">
+				<div class="row">
+					<div class="col-xs-12">
+					
+					<form action="lotteryclassify/${msg }.do" name="Form" id="Form" method="post">
+						<input type="hidden" name="lottery_classify_id" id="lottery_classify_id" value="${pd.lottery_classify_id}"/>
+						<div id="zhongxin" style="padding-top: 13px;">
+						<table id="table_report" class="table table-striped table-bordered table-hover">
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">彩种名称:</td>
+								<td><input type="text" name="lottery_name" id="lottery_name" value="${pd.lottery_name}" maxlength="50" placeholder="这里输入彩种名称" title="彩种名称" style="width:98%;"/></td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">彩种logo:</td>
+								<td>
+									<input name="lottery_img" class="hidden"   id="lottery_img" value="${pd.lottery_img}" />
+                            		<input  type="file" id="fileUpload" name="file"  onchange="ajaxFileUpload(this,'fileUpload')" style="display:none"/>
+										<div class="col-sm-9" style="float:left">
+									 	  <span class="btn btn-mini btn-primary" onclick="$('#fileUpload').trigger('click');"> 图片上传</span>  
+										</div>
+										<div class="col-sm-9"  style="float:left"> 
+											<img id="photoShow"  src="${pd.lottery_img}" width="24px",hight="24px"  >
+										</div>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">排序:</td>
+								<td><input type="number" name="sort" id="sort" value="${pd.sort}" maxlength="32" placeholder="这里输入排序" title="排序" style="width:98%;"  /></td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">状态:</td>
+								<td>
+								    <select  name="status" id="status" value="${pd.status}" style="width:98%;">
+								        <option value="" >请选择</option>
+								        <option value="0" <c:if test="${pd.status == 0}">selected</c:if>>售卖</option>
+								        <option value="1" <c:if test="${pd.status == 1}">selected</c:if>>停售</option>
+								    </select>
+								</td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">是否显示:</td>
+								<td>
+									<select  name="is_show" id="is_show" value="${pd.is_show}" style="width:98%;">
+								        <option value="" >请选择</option>
+								        <option value="0" <c:if test="${pd.is_show == 0}">selected</c:if>>不显示</option>
+								        <option value="1" <c:if test="${pd.is_show == 1}">selected</c:if>>显示</option>
+								    </select>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">跳转链接:</td>
+								<td><input type="text" name="redirect_url" id="redirect_url" value="${pd.redirect_url}" maxlength="256" placeholder="这里输入跳转链接" title="跳转链接" style="width:98%;"/></td>
+							</tr>
+							<tr>
+								<td style="text-align: center;" colspan="10">
+									<a class="btn btn-mini btn-primary" onclick="save();">保存</a>
+									<a class="btn btn-mini btn-danger" onclick="top.Dialog.close();">取消</a>
+								</td>
+							</tr>
+						</table>
+						</div>
+						<div id="zhongxin2" class="center" style="display:none"><br/><br/><br/><br/><br/><img src="static/images/jiazai.gif" /><br/><h4 class="lighter block green">提交中...</h4></div>
+					</form>
+					</div>
+					<!-- /.col -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.page-content -->
+		</div>
+	</div>
+	<!-- /.main-content -->
+</div>
+<!-- /.main-container -->
+
+
+	<!-- 页面底部js¨ -->
+	<%@ include file="../../system/index/foot.jsp"%>
+	<!-- 下拉框 -->
+	<script src="static/ace/js/chosen.jquery.js"></script>
+	<!-- 日期框 -->
+	<script src="static/ace/js/date-time/bootstrap-datepicker.js"></script>
+	<!--提示框-->
+	<script type="text/javascript" src="static/js/jquery.tips.js"></script>
+  	<!-- 文件上传 -->
+    <script src="plugins/fhform/js/ajaxfileupload.js"></script>
+		<script type="text/javascript">
+		$(top.hangge());
+		//保存
+		function save(){
+			if($("#lottery_name").val()==""){
+				$("#lottery_name").tips({
+					side:3,
+		            msg:'请输入彩种名称',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#lottery_name").focus();
+			return false;
+			}
+			if($("#lottery_img").val()==""){
+				$("#lottery_img").tips({
+					side:3,
+		            msg:'请输入彩种logo',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#lottery_img").focus();
+			return false;
+			}
+			if($("#sort").val()==""){
+				$("#sort").tips({
+					side:3,
+		            msg:'请输入排序',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#sort").focus();
+			return false;
+			}
+			if($("#status").val()==""){
+				$("#status").tips({
+					side:3,
+		            msg:'请输入状态',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#status").focus();
+			return false;
+			}
+			if($("#is_show").val()==""){
+				$("#is_show").tips({
+					side:3,
+		            msg:'请输入是否展示',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#is_show").focus();
+			return false;
+			}
+			if($("#redirect_url").val()==""){
+				$("#redirect_url").tips({
+					side:3,
+		            msg:'请输入跳转链接',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#redirect_url").focus();
+			return false;
+			}
+			$("#Form").submit();
+			$("#zhongxin").hide();
+			$("#zhongxin2").show();
+		}
+		
+		$(function() {
+			//日期框
+			$('.date-picker').datepicker({autoclose: true,todayHighlight: true});
+		});
+		
+		
+		//上传图片
+		function ajaxFileUpload(fileObj,fileId){
+		    $.ajaxFileUpload({
+		        url:'<%=basePath%>pictures/fileUpload.do',
+		        secureuri:false,                           //是否启用安全提交,默认为false
+		        fileElementId:fileId,               		//文件选择框的id属性
+		        dataType:'json',                           //服务器返回的格式,可以是json或xml等
+		        success:function(data, status){            //服务器响应成功时的处理函数
+		            if(data.result){
+			            	$("#photoShow").attr("src", data.IMG_SHOW_PATH).attr("width","24px").attr("hight","24px");
+			            	$("#lottery_img").val(data.PATH);
+		            } 
+		        }
+		    });
+		  }
+		
+		</script>
+</body>
+</html>
