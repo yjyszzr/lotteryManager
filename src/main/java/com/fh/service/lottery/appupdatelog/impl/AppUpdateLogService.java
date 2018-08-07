e0a4ccdb81e266b4a4a5133ce2bdaacb476ae1de
yjyszzr <1053195207@qq.com>
2018-08-07 10:04:50 +0800

    modified
    Change-Id: I39f42895b2e46527d856586521784c433455cf9e
    Signed-off-by: yjyszzr <1053195207@qq.com>

---
+++ src/main/java/com/fh/service/lottery/appupdatelog/impl/AppUpdateLogService.java
@@ -0,0 +1,82 @@
+package com.fh.service.lottery.appupdatelog.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.fh.dao.DaoSupport;
+import com.fh.entity.Page;
+import com.fh.util.PageData;
+import com.fh.service.lottery.appupdatelog.AppUpdateLogManager;
+
+/** 
+ * 说明： 版本升级管理
+ * 创建人：FH Q313596790
+ * 创建时间：2018-08-06
+ * @version
+ */
+@Service("appupdatelogService")
+public class AppUpdateLogService implements AppUpdateLogManager{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception{
+		dao.save("AppUpdateLogMapper.save", pd);
+	}
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception{
+		dao.delete("AppUpdateLogMapper.delete", pd);
+	}
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception{
+		dao.update("AppUpdateLogMapper.edit", pd);
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("AppUpdateLogMapper.datalistPage", page);
+	}
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("AppUpdateLogMapper.listAll", pd);
+	}
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("AppUpdateLogMapper.findById", pd);
+	}
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("AppUpdateLogMapper.deleteAll", ArrayDATA_IDS);
+	}
+	
+}
+
