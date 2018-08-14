9d74f5f835136f26c0a3e401ae14817bd8b9df17
liweijie <liweijie@caixiaomi.com>
2018-08-14 17:44:44 +0800

    增加APP资讯渠道管理模块

---
+++ src/main/java/com/fh/controller/lottery/phonechannel/PhoneChannelController.java
@@ -0,0 +1,283 @@
+package com.fh.controller.lottery.phonechannel;
+
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.service.lottery.articleclassify.ArticleClassifyManager;
+import com.fh.service.lottery.phonechannel.PhoneChannelManager;
+import com.fh.util.AppUtil;
+import com.fh.util.Jurisdiction;
+import com.fh.util.ObjectExcelView;
+import com.fh.util.PageData;
+
+/**
+ * 说明：APP渠道管理 创建人：FH Q313596790 创建时间：2018-08-14
+ */
+@Controller
+@RequestMapping(value = "/phonechannel")
+public class PhoneChannelController extends BaseController {
+
+	String menuUrl = "phonechannel/list.do"; // 菜单地址(权限用)
+	@Resource(name = "phonechannelService")
+	private PhoneChannelManager phonechannelService;
+	@Resource(name = "articleclassifyService")
+	private ArticleClassifyManager articleclassifyService;
+
+	/**
+	 * 保存
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/save")
+	public ModelAndView save() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "新增PhoneChannel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "add")) {
+			return null;
+		} // 校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		// pd.put("_id", this.get32UUID()); //主键
+		pd.put("id", "0"); // id
+		phonechannelService.save(pd);
+		mv.addObject("msg", "success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+
+	/**
+	 * 删除
+	 * 
+	 * @param out
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/delete")
+	public void delete(PrintWriter out) throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "删除PhoneChannel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "del")) {
+			return;
+		} // 校验权限
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		phonechannelService.delete(pd);
+		out.write("success");
+		out.close();
+	}
+
+	/**
+	 * 修改
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/edit")
+	public ModelAndView edit() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "修改PhoneChannel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "edit")) {
+			return null;
+		} // 校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		phonechannelService.edit(pd);
+		mv.addObject("msg", "success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+
+	/**
+	 * 列表
+	 * 
+	 * @param page
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/list")
+	public ModelAndView list(Page page) throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "列表PhoneChannel");
+		// if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;}
+		// //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = pd.getString("keywords"); // 关键词检索条件
+		if (null != keywords && !"".equals(keywords)) {
+			pd.put("keywords", keywords.trim());
+		}
+		page.setPd(pd);
+		List<PageData> varList = phonechannelService.list(page); // 列出PhoneChannel列表
+		List<PageData> articleclassifyList = articleclassifyService.listAll(pd);
+		Map<String, PageData> pageDataMap = new HashMap<String, PageData>();
+		articleclassifyList.forEach(s -> pageDataMap.put(s.getString("id"), s));
+		mv.setViewName("lottery/phonechannel/phonechannel_list");
+		for (int i = 0; i < varList.size(); i++) {
+			PageData pageData = varList.get(i);
+			if (!pageData.getString("article_classify_ids").equals("0")) {
+				List<PageData> articleclassifys = new ArrayList<PageData>();
+				String str = pageData.getString("article_classify_ids");
+				List<String> resultStr = Arrays.asList(str.split(","));
+				for (int j = 0; j < resultStr.size(); j++) {
+					PageData resultPageData = pageDataMap.get(resultStr.get(j));
+					if (resultPageData != null) {
+						articleclassifys.add(resultPageData);
+					}
+				}
+				varList.get(i).put("articleclassifyList", articleclassifys);
+			} else {
+				varList.get(i).put("articleclassifyList", articleclassifyList);
+			}
+		}
+		mv.addObject("varList", varList);
+		mv.addObject("pd", pd);
+		mv.addObject("QX", Jurisdiction.getHC()); // 按钮权限
+		return mv;
+	}
+
+	/**
+	 * 去新增页面
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/goAdd")
+	public ModelAndView goAdd() throws Exception {
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		mv.setViewName("lottery/phonechannel/phonechannel_edit");
+		List<PageData> articleclassifyList = articleclassifyService.listAll(pd);
+		mv.addObject("articleclassifyList", articleclassifyList);
+		mv.addObject("msg", "save");
+		mv.addObject("pd", pd);
+		return mv;
+	}
+
+	/**
+	 * 去修改页面
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/goEdit")
+	public ModelAndView goEdit() throws Exception {
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd = phonechannelService.findById(pd); // 根据ID读取
+		mv.setViewName("lottery/phonechannel/phonechannel_edit");
+		String str = pd.getString("article_classify_ids");
+		Map<String, PageData> pageDataMap = new HashMap<String, PageData>();
+
+		List<PageData> articleclassifyList = articleclassifyService.listAll(pd);
+		articleclassifyList.forEach(s -> pageDataMap.put(s.getString("id"), s));
+		List<String> resultStr = Arrays.asList(str.split(","));
+		for (int i = 0; i < articleclassifyList.size(); i++) {
+			PageData resultPageData = articleclassifyList.get(i);
+			articleclassifyList.get(i).put("isCheck", 0);
+			for (int j = 0; j < resultStr.size(); j++) {
+				if (resultPageData.getString("id").equals(resultStr.get(j))) {
+					articleclassifyList.get(i).put("isCheck", 1);
+				}
+			}
+		}
+		mv.addObject("articleclassifyList", articleclassifyList);
+		mv.addObject("msg", "edit");
+		mv.addObject("pd", pd);
+		return mv;
+	}
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/deleteAll")
+	@ResponseBody
+	public Object deleteAll() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "批量删除PhoneChannel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "del")) {
+			return null;
+		} // 校验权限
+		PageData pd = new PageData();
+		Map<String, Object> map = new HashMap<String, Object>();
+		pd = this.getPageData();
+		List<PageData> pdList = new ArrayList<PageData>();
+		String DATA_IDS = pd.getString("DATA_IDS");
+		if (null != DATA_IDS && !"".equals(DATA_IDS)) {
+			String ArrayDATA_IDS[] = DATA_IDS.split(",");
+			phonechannelService.deleteAll(ArrayDATA_IDS);
+			pd.put("msg", "ok");
+		} else {
+			pd.put("msg", "no");
+		}
+		pdList.add(pd);
+		map.put("list", pdList);
+		return AppUtil.returnObject(pd, map);
+	}
+
+	/**
+	 * 导出到excel
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/excel")
+	public ModelAndView exportExcel() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "导出PhoneChannel到excel");
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "cha")) {
+			return null;
+		}
+		ModelAndView mv = new ModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		List<String> titles = new ArrayList<String>();
+		titles.add("id"); // 1
+		titles.add("渠道编号"); // 2
+		titles.add("渠道名称"); // 3
+		titles.add("app编码"); // 4
+		titles.add("资讯类型"); // 5
+		dataMap.put("titles", titles);
+		List<PageData> varOList = phonechannelService.listAll(pd);
+		List<PageData> varList = new ArrayList<PageData>();
+		for (int i = 0; i < varOList.size(); i++) {
+			PageData vpd = new PageData();
+			vpd.put("var1", varOList.get(i).get("id").toString()); // 1
+			vpd.put("var2", varOList.get(i).getString("channel")); // 2
+			vpd.put("var3", varOList.get(i).getString("channel_name")); // 3
+			vpd.put("var4", varOList.get(i).get("app_code_name").toString()); // 4
+			vpd.put("var5", varOList.get(i).getString("article_classify_ids")); // 5
+			varList.add(vpd);
+		}
+		dataMap.put("varList", varList);
+		ObjectExcelView erv = new ObjectExcelView();
+		mv = new ModelAndView(erv, dataMap);
+		return mv;
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format, true));
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/lottery/phonechannel/phonechannel_edit.jsp
@@ -0,0 +1,154 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<base href="<%=basePath%>">
+	<!-- 下拉框 -->
+	<link rel="stylesheet" href="static/ace/css/chosen.css" />
+	<!-- jsp文件头和头部 -->
+	<%@ include file="../../system/index/top.jsp"%>
+	<!-- 日期框 -->
+	<link rel="stylesheet" href="static/ace/css/datepicker.css" />
+</head>
+<body class="no-skin">
+<!-- /section:basics/navbar.layout -->
+<div class="main-container" id="main-container">
+	<!-- /section:basics/sidebar -->
+	<div class="main-content">
+		<div class="main-content-inner">
+			<div class="page-content">
+				<div class="row">
+					<div class="col-xs-12">
+					
+					<form action="phonechannel/${msg }.do" name="Form" id="Form" method="post">
+						<input type="hidden" name="id" id="id" value="${pd.id}"/>
+						<input type="hidden"  id="classify_ids"   name="article_classify_ids"  />
+						<div id="zhongxin" style="padding-top: 13px;">
+						<table id="table_report" class="table table-striped table-bordered table-hover">
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">渠道编号:</td>
+								<td><input type="text" name="channel" id="channel" value="${pd.channel}" maxlength="45" placeholder="这里输入渠道编号" title="渠道编号" style="width:98%;"/></td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">渠道名称:</td>
+								<td><input type="text" name="channel_name" id="channel_name" value="${pd.channel_name}" maxlength="60" placeholder="这里输入渠道名称" title="渠道名称" style="width:98%;"/></td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">app编码:</td>
+								<td><input type="number" name="app_code_name" id="app_code_name" value="${pd.app_code_name}" maxlength="32" placeholder="这里输入app编码" title="app编码" style="width:98%;"/></td>
+							</tr>
+							<tr>
+								<td style="width:75px;text-align: right;padding-top: 13px;">资讯类型:</td>
+								<td>
+									<c:forEach var="articleclassify" items="${articleclassifyList}" varStatus="row">	 	
+										<label style="margin: 5px">
+								 			 <input type="checkbox" value="${articleclassify.id}"  <c:if test="${articleclassify.isCheck ==1}"> checked="checked" </c:if>  name ="article_classify" id ="article_classify" >${articleclassify.classify_name} 
+								 		 </label>
+								 		 <c:if test="${(row.index+1) % 4 ==0 }">
+								 			<br>
+								 		 </c:if>
+									 </c:forEach>
+								</td>
+							</tr>
+							<tr>
+								<td style="text-align: center;" colspan="10">
+									<a class="btn btn-mini btn-primary" onclick="save();">保存</a>
+									<a class="btn btn-mini btn-danger" onclick="top.Dialog.close();">取消</a>
+								</td>
+							</tr>
+						</table>
+						</div>
+						<div id="zhongxin2" class="center" style="display:none"><br/><br/><br/><br/><br/><img src="static/images/jiazai.gif" /><br/><h4 class="lighter block green">提交中...</h4></div>
+					</form>
+					</div>
+					<!-- /.col -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.page-content -->
+		</div>
+	</div>
+	<!-- /.main-content -->
+</div>
+<!-- /.main-container -->
+
+
+	<!-- 页面底部js¨ -->
+	<%@ include file="../../system/index/foot.jsp"%>
+	<!-- 下拉框 -->
+	<script src="static/ace/js/chosen.jquery.js"></script>
+	<!-- 日期框 -->
+	<script src="static/ace/js/date-time/bootstrap-datepicker.js"></script>
+	<!--提示框-->
+	<script type="text/javascript" src="static/js/jquery.tips.js"></script>
+		<script type="text/javascript">
+		$(top.hangge());
+		//保存
+		function save(){
+			if($("#channel").val()==""){
+				$("#channel").tips({
+					side:3,
+		            msg:'请输入渠道编号',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#channel").focus();
+			return false;
+			}
+			if($("#channel_name").val()==""){
+				$("#channel_name").tips({
+					side:3,
+		            msg:'请输入渠道名称',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#channel_name").focus();
+			return false;
+			}
+			if($("#app_code_name").val()==""){
+				$("#app_code_name").tips({
+					side:3,
+		            msg:'请输入app编码',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+				$("#app_code_name").focus();
+			return false;
+			}
+			
+			   var obj = document.getElementsByName("article_classify"); 
+			      var ids=''; 
+			      for(var i=0;i<obj.length;i++){
+			         if(obj[i].checked){
+			        	 ids+=obj[i].value+',';   
+			         }
+			      }
+			      $("#classify_ids").val(ids);
+			if($("#classify_ids").val()==""){
+				$("#article_classify").tips({
+					side:3,
+		            msg:'请选择资讯类型',
+		            bg:'#AE81FF',
+		            time:2
+		        });
+			return false;
+			}
+			$("#Form").submit();
+			$("#zhongxin").hide();
+			$("#zhongxin2").show();
+		}
+		
+		$(function() {
+			//日期框
+			$('.date-picker').datepicker({autoclose: true,todayHighlight: true});
+		});
+		</script>
+</body>
+</html>
